1c90aa9a87aada9e33abcae15b38f6aa4c76cc00
toe-recyglo <86629533+toe-recyglo@users.noreply.github.com>
2023-08-09 14:42:33 +0700

    Update cors

--- index.js
+++ index.js
@@ -75,14 +75,6 @@ global.api = "/api/v1";
 global.models = `${global.appRoot}/models`;
 global.controllers = `${global.appRoot}/controllers`;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.recyglo.net');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 // CORS
 app.use(cors());
 
